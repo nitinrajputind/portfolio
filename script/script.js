c68c430a7d36bad5fad0dd2b170cf77eff50b77d
nitinrajputind <nitinrajput971625@gmail.com>
2023-04-02 18:58:34 +0530

    Add media Query to make responsive

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
-// Scroll sections active link
+/*=======================Toggle icon navbar=============================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+/*=======================Scroll sections active link=============================*/
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
@@ -23,4 +35,10 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+/*=========remove toggle icon and navbar when click navbar link (scrol)=======================*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
 };
+
